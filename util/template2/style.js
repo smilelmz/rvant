ed337290a096d2f9fec080a68e1f466199f30a57
liangmz <1014241125@qq.com>
2021-06-23 16:02:21 +0800

    fix🐛:  update template generate utils

--- util/template2/style.js
+++ util/template2/style.js
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 module.exports = (componentName, _oName) => ({
-    content: `@import '../../components/style/vars.scss';
+  content: `@import '../../components/style/vars.scss';
+
 .demo-${_oName.toLowerCase()} {
 }
     `,
-    extension: `index.scss`
-  })
-  
+  extension: `index.scss`
+})
